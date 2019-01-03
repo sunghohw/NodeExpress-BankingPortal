8dfaa1e2b0d15f710ad2015c2956bb824ffc37c6
Sung Ho <sunghohw@andrew.cmu.edu>
2019-01-02 23:57:08 -0800

    add transfer GET route

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const userData = fs.readFileSync(path.join(__dirname,'json','users.json'),'utf8'
 const users = JSON.parse(userData);
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req,res) =>{
     res.render('index',{title: 'Account Summary',accounts})
 });
@@ -28,6 +29,9 @@ app.get('/credit', (req,res) =>{
 });
 app.get('/profile',(req,res) => {
     res.render('profile',{user: users[0]});
+});
+app.get('/transfer',(req,res) => {
+    res.render('transfer');
 });
 app.listen(3000,() => {
     console.log('PS Project Running on port 3000');
